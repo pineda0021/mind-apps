7722503c9b487b5b5939815b73f513e685428d7a
Edward Pineda-Castro <118245436+pineda0021@users.noreply.github.com>
2025-08-14 11:37:12 -0700

    Update statsmain.py

--- mind-stats-app/statsmain.py
+++ mind-stats-app/statsmain.py
@@ -10,10 +10,12 @@ import inferences_one_sample_tool
 import inferences_two_sample_tool  
 import chi_square_tests_tool
 import anova_tool
-import regression_analysis_tool 
+import regression_analysis_tool  
+
 
 st.set_page_config(page_title="MIND: Statistics Visualizer", layout="wide")
 st.title("🧠 MIND: Statistics Visualizer Suite")
+
 
 st.sidebar.header("📚 Select a Concept")
 tool = st.sidebar.radio("Choose a tool:", [
@@ -30,29 +32,40 @@ tool = st.sidebar.radio("Choose a tool:", [
     "Multiple Regression"
 ])
 
-# Routing each tool
+
 if tool == "Descriptive Statistics":
     descriptive_tool.run()
+
 elif tool == "Probability":     
     probability_tool.run()
+
 elif tool == "Discrete Distributions":
     discrete_dist_tool.run()
+
 elif tool == "Continuous Distributions":
     continuous_dist_tool.run()
+
 elif tool == "Confidence Intervals":   
     confidence_intervals_tool.run()
+
 elif tool == "Inferences on One Sample":
     inferences_one_sample_tool.run_hypothesis_tool() 
+
 elif tool == "Inferences on Two Samples":   
     inferences_two_sample_tool.run_two_sample_tool()  
+
 elif tool == "Chi-Square Tests":
     chi_square_tests_tool.run_chi_square_tool()  
+
 elif tool == "One-Way ANOVA":  
     anova_tool.run_anova_tool()  
+
 elif tool == "Simple Regression":
     regression_analysis_tool.run_simple_regression_tool()
+
 elif tool == "Multiple Regression":
     regression_analysis_tool.run_multiple_regression_tool()
+
 
 st.markdown("""
 ---
